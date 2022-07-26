339f2de9d6e4156564dcbb781f141d2f7888717a
Mac <mac@Mac-2.local>
2022-07-27 01:02:45 +0700

    modified: does not complie error

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -6,20 +6,13 @@
 /*   By: mac <sorungta@42bangkok.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 23:00:30 by mac               #+#    #+#             */
-/*   Updated: 2022/07/26 23:00:33 by mac              ###   ########.fr       */
+/*   Updated: 2022/07/27 01:02:24 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_alphabet(void)
+void	ft_putchar(char c)
 {
-	char	alpha;
-
-	alpha = 'a';
-	while (alpha <= 'z')
-	{
-		write(1, &alpha, 1);
-		alpha++;
-	}
+	write(1, &c, 1);
 }
